91bbf89bba5fee712ebd87ea2d813b56caee1112
Gaurav Rudravaram <gaurav.rudravaram@gmail.com>
2020-09-02 22:47:26 +0530

    Added all the files necessary for the website to work

---
+++ index.js
@@ -0,0 +1,65 @@
+var i=0;
+for(i=0;i<7;i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    var button=this.innerHTML;
+    sound(button);
+    animation(button);
+  });
+}
+
+document.addEventListener("keydown",function(event){
+  sound(event.key);
+  animation(event.key);
+});
+
+function sound(key){
+  switch (key) {
+    case "w":
+      var crash=new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "a":
+      var kick=new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case "s":
+      var snare=new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "d":
+      var tom1=new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "j":
+      var tom2=new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "k":
+      var tom3=new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "l":
+      var tom4=new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    default:console.log(button);
+
+  }
+}
+
+function animation(eve){
+  var active=document.querySelector("."+eve);
+  active.classList.add("pressed");
+  setTimeout(function(){
+      active.classList.remove("pressed");
+  },300);
+}
+// var audio=new Audio("sounds/crash.mp3");
+// audio.play();
